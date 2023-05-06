2dc78350ad16bfeca6a118eaa773a13f660a288a
Daniel Sage <sagedanielr@gmail.com>
2023-05-05 20:10:12 -0400

    Create sapitest package

--- sapitest/helpers_test.go
+++ sapitest/helpers_test.go
@@ -1,31 +1,32 @@
-package sapi
+package sapitest
 
 import (
+	"github.com/dnsge/gin-standard-api"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
 )
 
 func TestGetResStatus(t *testing.T) {
-	res := RawStatus(http.StatusCreated)
+	res := sapi.RawStatus(http.StatusCreated)
 	assert.Equal(t, http.StatusCreated, GetResStatus(res))
-	res = Status(http.StatusCreated)
+	res = sapi.Status(http.StatusCreated)
 	assert.Equal(t, http.StatusCreated, GetResStatus(res))
 }
 
 func TestGetResBody(t *testing.T) {
-	res := Status(http.StatusCreated)
+	res := sapi.Status(http.StatusCreated)
 	assert.JSONEq(t, `{"status": 201}`, GetResBody(res))
 }
 
 func TestGetErrStatus(t *testing.T) {
-	err := RawErrorStatus(http.StatusInternalServerError)
+	err := sapi.RawErrorStatus(http.StatusInternalServerError)
 	assert.Equal(t, http.StatusInternalServerError, GetErrStatus(err))
-	err = ErrorStatus(http.StatusInternalServerError)
+	err = sapi.ErrorStatus(http.StatusInternalServerError)
 	assert.Equal(t, http.StatusInternalServerError, GetErrStatus(err))
 }
 
 func TestGetErrBody(t *testing.T) {
-	err := Error(http.StatusInternalServerError, "Oh no")
+	err := sapi.Error(http.StatusInternalServerError, "Oh no")
 	assert.JSONEq(t, `{"status": 500, "error": "Oh no"}`, GetErrBody(err))
 }
--- sapitest/test_helpers.go
+++ sapitest/test_helpers.go
@@ -1,12 +1,13 @@
-package sapi
+package sapitest
 
 import (
+	"github.com/dnsge/gin-standard-api"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"net/http/httptest"
 )
 
-func execRes(res Res) *httptest.ResponseRecorder {
+func execRes(res sapi.Res) *httptest.ResponseRecorder {
 	recorder := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(recorder)
 	res.WriteResponse(c)
@@ -15,7 +16,7 @@ func execRes(res Res) *httptest.ResponseRecorder {
 	return recorder
 }
 
-func execErr(err Err) *httptest.ResponseRecorder {
+func execErr(err sapi.Err) *httptest.ResponseRecorder {
 	recorder := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(recorder)
 	err.WriteError(c)
@@ -26,42 +27,42 @@ func execErr(err Err) *httptest.ResponseRecorder {
 
 // GetResStatus returns the status code contained within the given response.
 // This method should be exclusively used for testing.
-func GetResStatus(res Res) int {
+func GetResStatus(res sapi.Res) int {
 	recorder := execRes(res)
 	return recorder.Code
 }
 
 // GetResHeaders returns the headers written for the given response.
 // This method should be exclusively used for testing.
-func GetResHeaders(res Res) http.Header {
+func GetResHeaders(res sapi.Res) http.Header {
 	recorder := execRes(res)
 	return recorder.Header()
 }
 
 // GetResBody returns the body written for the given response.
 // This method should be exclusively used for testing.
-func GetResBody(res Res) string {
+func GetResBody(res sapi.Res) string {
 	recorder := execRes(res)
 	return recorder.Body.String()
 }
 
 // GetErrStatus returns the status code contained within the given error response.
 // This method should be exclusively used for testing.
-func GetErrStatus(err Err) int {
+func GetErrStatus(err sapi.Err) int {
 	recorder := execErr(err)
 	return recorder.Code
 }
 
 // GetErrHeaders returns the headers written for the given error response.
 // This method should be exclusively used for testing.
-func GetErrHeaders(err Err) http.Header {
+func GetErrHeaders(err sapi.Err) http.Header {
 	recorder := execErr(err)
 	return recorder.Header()
 }
 
 // GetErrBody returns the body written for the given error response.
 // This method should be exclusively used for testing.
-func GetErrBody(err Err) string {
+func GetErrBody(err sapi.Err) string {
 	recorder := execErr(err)
 	return recorder.Body.String()
 }
